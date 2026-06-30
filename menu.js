769cd53984726064592b07c409958a5744ef388f
julia oliveira <oliveiramartinsjulia7@gmail.com>
2026-06-30 18:14:52 -0300

    Delete menu.js

--- menu.js
+++
@@ -1,8 +0,0 @@
- const btn = document.getElementById('menuToggle');
-    const esquerda = document.getElementById('menuEsquerda');
-    const direita = document.getElementById('menuDireita');
-
-    btn.addEventListener('click', () => {
-      esquerda.classList.toggle('aberto');
-      direita.classList.toggle('aberto');
-    });
